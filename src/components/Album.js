476e1b92a443531132e7f95e13a45a97500213d0
NQN6-5 <dcastoral@outlook.com>
2019-04-15 16:03:05 -0400

    commitplayerbarbuttons

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
-
+import PlayerBar from './PlayerBar';
 
 
 class Album extends Component {
@@ -56,8 +56,17 @@ class Album extends Component {
     handleSongRowHover(index) {
         this.setState({hovered: index})
 
+            }
 
+    handlePrevClick() {
+        const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+        const newIndex = Math.max(0, currentIndex - 1);
+        const newSong = this.state.album.songs[newIndex];
+        this.setSong(newSong);
+        this.play();
     }
+
+
 
     render() {
         return (
@@ -95,8 +104,12 @@ class Album extends Component {
                     </tbody>
 
                 </table>
-
-
+                <PlayerBar
+                    isPlaying={this.state.isPlaying}
+                    currentSong={this.state.currentSong}
+                    handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+                    handlePrevClick={() => this.handlePrevClick()}
+                />
                 </section>
 
         );
